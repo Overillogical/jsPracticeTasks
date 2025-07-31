4b5505c2cfb50dd81ab08aef33eec86c35d461a4
AlexanderS <ksellos99@gmail.com>
2025-07-31 19:12:13 +0300

    Adding gold feature, 3 functions that can build , increased gold amount and decrease it

---
+++ lessons/lesson-07/main.js
@@ -0,0 +1,19 @@
+let gold = 100;
+function createBuilding (buildingName, goldCost){
+    const hasEnoughGold = gold>= goldCost;
+    if (hasEnoughGold) {
+        reduceResource(gold,goldCost);
+        console.log(`${buildingName}: work complete!`);
+    } else {
+        console.log(`${buildingName}: not enough resources!`);
+    }
+}
+function increaseResource(currentAmount, increment){
+    return currentAmount+increment;
+}
+function reduceResource(currentAmount, increment){
+    return currentAmount - increment;
+}
+createBuilding('Tower',  30);
+createBuilding('House', 140);
+console.log(gold);
