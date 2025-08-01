5c21428244036551cff787832c7c94aaef26134c
AlexanderS <ksellos99@gmail.com>
2025-08-01 11:33:34 +0300

    Added code to generate random phrase from 3 initial arrays

--- lessons/lesson18/main.js
+++ lessons/lesson18/main.js
@@ -1,3 +1,5 @@
 const words1= ['Мой кот','Моя собака','Мой попугай'];
 const words2 = ['любит есть', 'хочет погрызть','всегда ищет'];
 const words3 =['морковку','макароны','косточку'];
+
+console.log(`${words1[Math.floor(Math.random()*words1.length)]} ${words2[Math.floor(Math.random()*words2.length)]} ${words3[Math.floor(Math.random()*words3.length)]}`);
