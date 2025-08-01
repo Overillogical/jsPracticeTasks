99ddfececd48e75974251bc8b848dbc421712a10
AlexanderS <ksellos99@gmail.com>
2025-08-01 11:48:15 +0300

    added function to choose random phrase from array to simplify and shorten the code

--- lessons/lesson19/main.js
+++ lessons/lesson19/main.js
@@ -2,10 +2,9 @@ const words1 = ['Мой кот', 'Моя собака', 'Мой попугай']
 const words2 = ['любит есть', 'хочет погрызть', 'всегда ищет'];
 const words3 = ['морковку', 'макароны', 'косточку'];
 function makePhrase() {
-    const randomIndex_1 = Math.floor(Math.random() * words1.length);
-    const randomIndex_2 = Math.floor(Math.random() * words2.length);
-    const randomIndex_3 = Math.floor(Math.random() * words3.length);
-    const phrase = `${words1[randomIndex_1]} ${words2[randomIndex_2]} ${words3[randomIndex_3]}`;
-    return phrase;
+    return `${choosePhraseFromArray(words1)} ${choosePhraseFromArray(words2)} ${choosePhraseFromArray(words3)}`;
+}
+function choosePhraseFromArray(array){
+    return array[Math.floor(Math.random() * array.length)];
 }
 console.log(makePhrase());
