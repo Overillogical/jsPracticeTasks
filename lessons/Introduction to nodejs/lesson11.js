8c6c2256718a360d81dbfa278fc1a772cdbb5bb8
AlexanderS <ksellos99@gmail.com>
2025-08-26 21:13:04 +0300

    Added comments for code clarity

--- lessons/Introduction to nodejs/lesson11.js
+++ lessons/Introduction to nodejs/lesson11.js
@@ -1,18 +1,25 @@
 import http from 'http'
+//Array with users
 const users = [
     { id: 1, name: "Alice", age: 25 },
     { id: 2, name: "Bob", age: 30 },
     { id: 3, name: "Charlie", age: 22 }
 ];
+
+//Variable to store count of requests to specific routes
 let aboutRequests =0;
 let helloRequests =0;
 let usersRequests=0;
+//Function to log time when route was acessed
 const logTime = (url) =>{
     console.log(`[GET] ${url} at ${new Date().toISOString()}`)
 }
+//Server creation
 const server = http.createServer((req,res)=>{
     let totalRequests = aboutRequests+helloRequests+usersRequests;
+    // If statement to check if user tries to access to specific user page
     if(req.url.startsWith("/users/")){
+    // If statement to check if the user that is getting accessed exists
         if(((+req.url.slice(7))>0&&(+req.url.slice(7))<=users.length)){
             logTime(req.url)
             usersRequests+=1;
@@ -21,6 +28,7 @@ const server = http.createServer((req,res)=>{
     }else
         res.end("User not found")
     }else
+    //Handling all other requests, except when user accesses specific user from /users/
     switch (req.url){
         case '/hello':
             logTime(req.url)
